3d7f079d962409a00a8bdcb2f802572b80eae2b9
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-03-26 12:18:09 +0300

    [ft]-variables-data-types

---
+++ variables_data_types.js
@@ -0,0 +1,53 @@
+
+//alert('caleb is learning JS');
+//console.log('Hello World');
+//console.error('This is an error');
+//console.warn('This is a warning');
+
+//(b) Variables
+
+//let age =30;
+//age =23;
+
+//console.log(age);
+
+//let score;
+//score =10;
+//console.log(score);
+
+
+//(c)Datatypes
+
+//const name ='Caleb';
+//const age =30;
+//const rating=4.5;
+//const iscool = true;
+//const x = null;
+//const y=undefined;
+//let z;
+
+//console.log(typeof x);
+
+const name ='Caleb';
+const age =30;
+
+//Concatenation
+
+//console.log('My name is'+name+'and I am'+age);
+//ES6
+
+//Template String
+console.log(`My name is ${name} and I am ${age}`);
+
+const hello = `My name is ${name} and I am ${age}`;
+
+console.log(hello);
+
+
+
+
+
+
+
+
+
