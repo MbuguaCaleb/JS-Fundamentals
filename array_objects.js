af7c5dc0c22e06618b0b8207bb6cc40612b87a48
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-04-01 16:52:23 +0300

    adding array with objects

---
+++ array_objects.js
@@ -0,0 +1,31 @@
+
+//arrays of objects(dictionaries)
+
+const todos =[
+
+   {
+       id:1,
+       text:' Node.js',
+       isCompleted:true
+   },
+
+   {
+    id:2,
+    text:'JS',
+    isCompleted:true
+   },
+
+  { 
+    id:3,
+    text:'Laravel',
+    isCompleted:true
+  }
+
+];
+
+//console.log(todos);
+console.log(todos[1].text);
+
+
+
+
